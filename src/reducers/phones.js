3a45be86837d5e69d52a191738f02a311ccf781f
Kuharenko <iamksergey@mail.ru>
2020-03-11 16:37:50 +0300

    master

--- src/reducers/phones.js
+++ src/reducers/phones.js
@@ -1,15 +1,23 @@
-import * as R from 'ramda'
 import {FETCH_PHONES_SUCCESS} from '../actionTypes'
 
-const initialState = {}
+const initialState = {};
+
+const convert = (array, key) => array.reduce((result, item,) => {
+    result[item[key]] = item;
+    return result
+}, {});
+
+const merge = (state, newState) => {
+    return {...state, ...newState}
+};
 
 export default (state = initialState, {type, payload}) => {
     switch (type) {
         case FETCH_PHONES_SUCCESS:
-            const newValues = R.indexBy(R.prop('id'), payload)
-            console.log(payload)
-            console.log(newValues)
-            return R.merge(state, newValues)
+            // конвертируем массив объектов в объект объектов с ключами 'id' // 1: {}, 2: {}, ...
+            const newValues = convert(payload, 'id');
+            return merge(state, newValues);
+
         default:
             return state
     }
